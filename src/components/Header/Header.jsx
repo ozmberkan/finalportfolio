8391f5dc202a77f78c851f90ce4aa70e56e0c363
ozmberkan <ozmberkan@gmail.com>
2024-10-09 00:02:29 +0300

    tooltip eklendi.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import blur from "~/assets/blur/blur.svg";
 import { useSelector } from "react-redux";
 import { signOut } from "firebase/auth";
 import { auth } from "~/firebase/firebase";
+import { Tooltip } from "react-tooltip";
 
 const Header = () => {
   const { user } = useSelector((store) => store.user);
@@ -19,65 +20,71 @@ const Header = () => {
   };
 
   return (
-    <div className="w-full sm:h-[300px] flex  justify-start items-start p-12 border-b border-zinc-500 relative">
-      <img src={blur} className="absolute top-0 -z-10  " />
-      <div className="flex gap-x-5 w-full sm:flex-row flex-col gap-y-5">
-        <img
-          src="./profile.jpeg"
-          alt="profile photo"
-          className="min-w-48 max-h-48 object-cover rounded-xl ring-2 ring-offset-4 ring-[#075400]/80 ring-offset-black "
-        />
-        <div className="flex flex-col sm:text-left text-center gap-y-4 w-full">
-          <div className="flex gap-y-1  w-full justify-between items-start sm:flex-row flex-col ">
-            <div className="flex flex-col">
-              <h1 className="text-4xl font-black">Muhammed Berkan Özmen</h1>
-              <p className="font-medium text-zinc-400">
-                jr. front-end developer / react developer
-              </p>
+    <>
+      <div className="w-full sm:h-[300px] flex  justify-start items-start p-12 border-b border-zinc-500 relative">
+        <img src={blur} className="absolute top-0 -z-10  " />
+        <div className="flex gap-x-5 w-full sm:flex-row flex-col gap-y-5">
+          <img
+            src="./profile.jpeg"
+            alt="profile photo"
+            className="min-w-48 max-h-48 object-cover rounded-xl ring-2 ring-offset-4 ring-[#075400]/80 ring-offset-black "
+          />
+          <div className="flex flex-col sm:text-left text-center gap-y-4 w-full">
+            <div className="flex gap-y-1  w-full justify-between items-start sm:flex-row flex-col ">
+              <div className="flex flex-col">
+                <h1 className="text-4xl font-black">Muhammed Berkan Özmen</h1>
+                <p className="font-medium text-zinc-400">
+                  jr. front-end developer / react developer
+                </p>
+              </div>
+              <div className="flex gap-x-5 sm:flex-row flex-col sm:w-auto w-full">
+                {navTabs.map((tab) => (
+                  <Link
+                    key={tab.id}
+                    to={tab.to}
+                    className="text-xl font-semibold text-zinc-400 hover:text-white transition-colors duration-500"
+                  >
+                    {tab.label}
+                  </Link>
+                ))}
+                {user && (
+                  <div className="flex gap-x-5">
+                    <Link
+                      to="/admin"
+                      className="text-xl font-semibold text-zinc-400 hover:text-white transition-colors duration-500"
+                    >
+                      Admin
+                    </Link>
+                    <button
+                      onClick={exit}
+                      className="text-xl font-semibold text-zinc-400 hover:text-white transition-colors duration-500"
+                    >
+                      Çıkış Yap
+                    </button>
+                  </div>
+                )}
+              </div>
             </div>
-            <div className="flex gap-x-5 sm:flex-row flex-col sm:w-auto w-full">
-              {navTabs.map((tab) => (
+            <div className="w-full flex gap-x-4">
+              {socialMedia.map((item) => (
                 <Link
-                  key={tab.id}
-                  to={tab.to}
-                  className="text-xl font-semibold text-zinc-400 hover:text-white transition-colors duration-500"
+                  key={item.id}
+                  to={item.to}
+                  data-tooltip-id="my-tooltip"
+                  data-tooltip-content={item.label}
+                  data-tooltip-place="bottom"
+                  target={item.to === "/resume" ? "_self" : "_blank"}
+                  className="p-2 rounded-full border-2 hover:ring-2 hover:border-zinc-500 hover:text-zinc-500 hover:ring-offset-black hover:ring-zinc-600 ring-offset-2 border-white text-2xl transition-all duration-500"
                 >
-                  {tab.label}
+                  <item.icon />
                 </Link>
               ))}
-              {user && (
-                <div className="flex gap-x-5">
-                  <Link
-                    to="/admin"
-                    className="text-xl font-semibold text-zinc-400 hover:text-white transition-colors duration-500"
-                  >
-                    Admin
-                  </Link>
-                  <button
-                    onClick={exit}
-                    className="text-xl font-semibold text-zinc-400 hover:text-white transition-colors duration-500"
-                  >
-                    Çıkış Yap
-                  </button>
-                </div>
-              )}
             </div>
-          </div>
-          <div className="w-full flex gap-x-4">
-            {socialMedia.map((item) => (
-              <Link
-                key={item.id}
-                to={item.to}
-                target={item.to === "/resume" ? "_self" : "_blank"}
-                className="p-2 rounded-full border-2 hover:ring-2 hover:border-zinc-500 hover:text-zinc-500 hover:ring-offset-black hover:ring-zinc-600 ring-offset-2 border-white text-2xl transition-all duration-500"
-              >
-                <item.icon />
-              </Link>
-            ))}
           </div>
         </div>
       </div>
-    </div>
+      <Tooltip id="my-tooltip" />
+    </>
   );
 };
 
--- src/data/data.js
+++ src/data/data.js
@@ -3,11 +3,27 @@ import { FaXTwitter } from "react-icons/fa6";
 import { SiReaddotcv } from "react-icons/si";
 
 export const socialMedia = [
-  { id: 1, icon: FaLinkedin, to: "https://www.linkedin.com/in/berkanozm/" },
-  { id: 2, icon: FaGithub, to: "https://github.com/ozmberkan" },
-  { id: 3, icon: FaInstagram, to: "https://www.instagram.com/berkanozm/" },
-  { id: 4, icon: FaXTwitter, to: "https://twitter.com/berkannozm" },
-  { id: 5, icon: SiReaddotcv, to: "/resume" },
+  {
+    id: 1,
+    icon: FaLinkedin,
+    to: "https://www.linkedin.com/in/berkanozm/",
+  },
+  {
+    id: 2,
+    icon: FaGithub,
+    to: "https://github.com/ozmberkan",
+  },
+  {
+    id: 3,
+    icon: FaInstagram,
+    to: "https://www.instagram.com/berkanozm/",
+  },
+  {
+    id: 4,
+    icon: FaXTwitter,
+    to: "https://twitter.com/berkannozm",
+  },
+  { id: 5, icon: SiReaddotcv, to: "/resume", label: "Özgeçmiş" },
 ];
 
 export const navTabs = [
